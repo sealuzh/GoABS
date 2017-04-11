cc91986efa1c0aa7497502abc7c67eeb55bd4587
Christoph Laaber <christoph.laaber@gmail.com>
2017-04-11 17:37:08 +0200

    fixed bug where failing test (e.g. due to orphaned goroutines) results are not parsed although the benchmarks were successful

--- bench/runner.go
+++ bench/runner.go
@@ -107,10 +107,14 @@ func (r *runnerWithPenalty) Run(run int, test string) (int, error) {
 				resStr := string(res)
 				if err != nil {
 					fmt.Printf("Error while executing command '%s\n", c.Args)
-					fmt.Printf("%s\n", resStr)
-					r.penalisedBenchs[relBenchName] = struct{}{}
-					err = nil
-					continue
+					if strings.Contains(resStr, benchTimeoutMsg) {
+						fmt.Printf("%s timed out after %s\n", relBenchName, r.timeout)
+						r.penalisedBenchs[relBenchName] = struct{}{}
+						err = nil
+						continue
+					} else {
+						fmt.Printf("%s\n", resStr)
+					}
 				}
 
 				err = parseAndSaveBenchOut(test, run, bench, pkgName, resStr, r.out)
