1cba2305779438aaf0880d86ff5a8209a48f9a87
Christoph Laaber <christoph.laaber@gmail.com>
2017-08-10 08:36:56 +0200

    changed regression introducer to using defer

--- trans/regression/regression.go
+++ trans/regression/regression.go
@@ -136,27 +136,21 @@ func (v *relRegVisitor) VisitFuncDecl(node *ast.FuncDecl) ast.Visitor {
 	}
 	// add start to begin of method body
 	list = append(list, start)
-	list = append(list, b.List...)
 
-	// add actual regression
+	// add deferred sleep
 	// time.Sleep(time.Duration(float64(time.Since(start).Nanoseconds()) * 0.1))
 	sleep := v.sleepStmt(time, startVarName)
-	// add sleep to end of body
-	lastStmt := b.List[len(b.List)-1]
-	list = append(list, sleep)
-	lenList := len(list)
-	switch lastStmt.(type) {
-	case *ast.ReturnStmt:
-		el := list[lenList-1]
-		list[lenList-1] = list[lenList-2]
-		list[lenList-2] = el
+	deferredSleep := &ast.DeferStmt{
+		Call: sleep,
 	}
+	list = append(list, deferredSleep)
 
+	list = append(list, b.List...)
 	b.List = list
 	return v
 }
 
-func (v *relRegVisitor) sleepStmt(timePkg, startVarName *ast.Ident) *ast.ExprStmt {
+func (v *relRegVisitor) sleepStmt(timePkg, startVarName *ast.Ident) *ast.CallExpr {
 	// duration
 	dur := &ast.CallExpr{
 		Fun: &ast.SelectorExpr{
@@ -217,7 +211,5 @@ func (v *relRegVisitor) sleepStmt(timePkg, startVarName *ast.Ident) *ast.ExprStm
 		},
 	}
 
-	return &ast.ExprStmt{
-		X: sleep,
-	}
+	return sleep
 }
